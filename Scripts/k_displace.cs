3ddaf13d9f1dca119ec7a7e1bf28454db97ac642
cravens1968 <44187035+cravens1968@users.noreply.github.com>
2021-05-15 17:17:06 -0700

    added params

--- Scripts/k_displace.cs
+++ Scripts/k_displace.cs
@@ -7,6 +7,7 @@ public class k_displace : ConsoleCmdAbstract
   {
       private EntityPlayer entityPlayer;
 	  private Vector3i newLocationV3i = new Vector3i(0,0,0);
+	  private int dx, dz;
 
 	  public override string[] GetCommands() => new[] {"k_displace"};
 
@@ -14,8 +15,17 @@ public class k_displace : ConsoleCmdAbstract
 
       public override void Execute(List<string> _params, CommandSenderInfo _senderInfo)
     {
-      
-		if (!_senderInfo.IsLocalGame && _senderInfo.RemoteClientInfo == null)
+		if (_params.Count == 2 )
+		{
+			
+			bool isValid = int.TryParse(_params[0], out dx);
+			if (isValid)
+            {
+				isValid = int.TryParse(_params[1], out dz);
+			}
+		}
+
+			if (!_senderInfo.IsLocalGame && _senderInfo.RemoteClientInfo == null)
 		{
 			SingletonMonoBehaviour<SdtdConsole>.Instance.Output("Command can only be used on clients.");
 			return;
@@ -25,11 +35,11 @@ public class k_displace : ConsoleCmdAbstract
 		if (_senderInfo.IsLocalGame)
 		{
 			entityPlayer = GameManager.Instance.World.GetPrimaryPlayer();
-			newLocationV3i = displaceEntity(100, 0, 100);
+			newLocationV3i = displaceEntity(dx, 0, dz);
 			chatOutput(string.Format("Current Player Coordinates: {0},{1}  -> New Coordinates {2}, {3}", entityPlayer.GetBlockPosition().x, entityPlayer.GetBlockPosition().z, newLocationV3i.x, newLocationV3i.z));
 			if (!SingletonMonoBehaviour<ConnectionManager>.Instance.IsClient)
 			{
-				SingletonMonoBehaviour<SdtdConsole>.Instance.ExecuteSync(buildConsoleCommand(newLocationV3i), null);
+				teleport(newLocationV3i);
 			}
 			else
 			{
@@ -49,12 +59,24 @@ public class k_displace : ConsoleCmdAbstract
 		Vector3i displaceAmount = new Vector3i(_x, _y, _z);
 		return entityPlayer.GetBlockPosition() + displaceAmount;
 	}
-	private string buildConsoleCommand(Vector3i _location)
+	private string buildConsoleCommand(Vector3i _location, bool _onground)
     {
-		string outputStr = string.Format("teleport {0} {1} ", _location.x,  _location.z);
+		string outputStr ="";
+
+		if(_onground)
+        {
+			outputStr = string.Format("teleport {0} {1} ", _location.x, _location.z);
+		} else
+        {
+			outputStr = string.Format("teleport {0} {1} {2}", _location.x, _location.y, _location.z);
+		}
+		
 		return outputStr;
     }
-
+	private void teleport(Vector3i targetLocation, bool onGround = true)
+    {
+		SingletonMonoBehaviour<SdtdConsole>.Instance.ExecuteSync(buildConsoleCommand(targetLocation, onGround), null);
+	}
 	private void chatOutput(string msg)
 	{
 		GameManager.Instance.ChatMessageServer(_cInfo: null, _chatType: EChatType.Global, _senderEntityId: entityPlayer.entityId, _msg: msg, _mainName: entityPlayer.EntityName, _localizeMain: false, _recipientEntityIds: null);
